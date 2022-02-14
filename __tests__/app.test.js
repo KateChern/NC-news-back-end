aa344b823e9705d9c98ef79e1b28fa6c55183e01
kate-del <chernyakova.c@gmail.com>
2022-02-14 15:34:11 +0000

    add get api/articles, error handlers and tests

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -35,4 +35,44 @@ describe("app", () => {
         });
     });
   });
+
+  describe("GET - /api/articles", () => {
+    test("status:200, responds with an array of articles objects", () => {
+      return request(app)
+        .get("/api/articles")
+        .expect(200)
+        .then(({ body: { articles } }) => {
+          expect(articles).toHaveLength(12);
+          expect(articles).toBeInstanceOf(Array);
+          articles.forEach((article) => {
+            expect(article).toEqual(
+              expect.objectContaining({
+                article_id: expect.any(Number),
+                title: expect.any(String),
+                topic: expect.any(String),
+                author: expect.any(String),
+                created_at: expect.any(String),
+                votes: expect.any(Number),
+              })
+            );
+          });
+        });
+    });
+    test("status:404, responds with a message 'Path not found' when there is an incorrect pathway", () => {
+      return request(app)
+        .get("/api/article")
+        .expect(404)
+        .then(({ body: { msg } }) => {
+          expect(msg).toBe("Path not found");
+        });
+    });
+    test("status:200, articles are sorted by date, descending order by default", () => {
+      return request(app)
+        .get("/api/articles")
+        .expect(200)
+        .then(({ body: { articles } }) => {
+          expect(articles).toBeSortedBy("created_at", { descending: true });
+        });
+    });
+  });
 });
