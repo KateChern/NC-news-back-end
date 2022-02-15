e9ee4d2e36112953c03938289d513347b91f761d
kate-del <chernyakova.c@gmail.com>
2022-02-15 17:02:54 +0000

    add test for invalid id

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -239,6 +239,7 @@ describe("app", () => {
         username: "lurker",
         body: "test",
       };
+
       return request(app)
         .post("/api/articles/4/comments")
         .send(testComment)
@@ -268,7 +269,20 @@ describe("app", () => {
         });
     });
   });
-  test("status:404, responds with a message 'invalid username or article' when article_id is valid but doesn't exist", () => {
+  test("status:400, responds with a message 'Bad request' for invalid article_id", () => {
+    const testComment = {
+      username: "lurker",
+      body: "test",
+    };
+    return request(app)
+      .post("/api/articles/not-id/comments")
+      .send(testComment)
+      .expect(400)
+      .then(({ body: { msg } }) => {
+        expect(msg).toBe("Bad request");
+      });
+  });
+  test("status:400, responds with a message 'invalid username or article' when article_id is valid but doesn't exist", () => {
     const testComment = {
       username: "lurker",
       body: "test",
