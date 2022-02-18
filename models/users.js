2148097e651d00aaaebb5afcd0b03792e52ceb56
kate-del <chernyakova.c@gmail.com>
2022-02-18 15:01:09 +0000

    add functions and  tests for GET - /api/users/:username

--- models/users.js
+++ models/users.js
@@ -5,3 +5,13 @@ exports.getUsers = async () => {
 
   return result.rows;
 };
+
+exports.getUserByName = async (userName) => {
+  const result = await db.query(`SELECT * FROM users WHERE username = $1;`, [
+    userName,
+  ]);
+  if (result.rows.length === 0) {
+    return Promise.reject({ status: 404, msg: "User not found" });
+  }
+  return result.rows[0];
+};
