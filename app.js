b127472bd1b23c93ad5a9f3b1641dd151d496418
kate-del <chernyakova.c@gmail.com>
2022-03-07 11:11:56 +0000

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+app.use(cors());
 const { getTopics } = require("./controllers/topics");
 const { getUsers, getUserByNameController } = require("./controllers/users");
 const { getEndpointsController } = require("./controllers/endpoints");
